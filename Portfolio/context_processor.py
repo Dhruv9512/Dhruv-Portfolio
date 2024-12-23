1227f5f0d131f91e1957972fe7b048275026a10c
Dhruv9512 <dhruvsharma56780@gmail.com>
2024-12-23 19:06:00 +0530

    Add the links

--- Portfolio/context_processor.py
+++ Portfolio/context_processor.py
@@ -6,15 +6,35 @@ from django.urls import reverse
 
 # pass the header and footer data
 def fotter_data(request):
+    # Get current year
+    from datetime import datetime
+    current_year = datetime.now().year
+
+    # Footer details
     address_data = Footer.objects.filter(title="Address").first()
     mobile_data = Footer.objects.filter(title="Mobile Number").first()
     Email_data = Footer.objects.filter(title="Email").first()
     Footer_About_Me_Data = Footer.objects.filter(title="Footer About Me").first()
+    leetcode_link = Footer.objects.filter(title="Leetcode").first()
+    hackerrank_link = Footer.objects.filter(title="Hackerrank").first()
+    linkedin_link = Footer.objects.filter(title="Linkedin").first()
+    github_link = Footer.objects.filter(title="Github").first()
+
+    # Portfolio Logo
     logo_data = MyImage.objects.filter(title="Profile Logo").first()
-    
 
     context = {
         "Image":{"Profile_Logo":logo_data},
-        "Footer":{"Address":address_data,"Mobile_Number":mobile_data,"Email":Email_data,"about_me":Footer_About_Me_Data}
+        "Footer":{
+            "Address":address_data,
+            "Mobile_Number":mobile_data,
+            "Email":Email_data,
+            "about_me":Footer_About_Me_Data,
+            "Leetcode":leetcode_link,
+            "Hackerrank":hackerrank_link,
+            "Linkedin":linkedin_link,
+            "Github":github_link
+        },
+        "Current_year":current_year
     }
     return context
