a4cd6d9f8b6eb4939f72789461359c41f0281b62
man-navlakha <dhruvsharma56780@gmail.com>
2025-03-15 00:54:26 +0530

    Allow Host

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -13,7 +13,7 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dhruv-portfolio-285z.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['https://dhruv-portfolio-jyx9.onrender.com/', '127.0.0.1', 'localhost']
 
 # Application definition
 INSTALLED_APPS = [
