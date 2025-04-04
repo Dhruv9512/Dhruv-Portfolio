5f5d2be5ccb8dadf3cfceec497e322cd05697972
man-navlakha <dhruvsharma56780@gmail.com>
2025-04-05 00:28:16 +0530

    sol

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -15,7 +15,8 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dhruv-portfolio-vqsr.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['dhruv-portfolio-n5a6.onrender.com', 'localhost', '127.0.0.1']
+
 
 # Application definition
 INSTALLED_APPS = [
